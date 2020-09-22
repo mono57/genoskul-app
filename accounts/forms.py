99fc2493d1f68ac1a97bcc97c83b384e9e53d73c
mono57 <aymaramono@gmail.com>
2020-09-22 16:20:36 +0100

    update home page

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,6 +7,9 @@ class RegisterStep2ModelForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ('user', 'avatar')
+        help_texts = {
+            'birthday': 'Entrez la date sous le format: <span class=\'text-primary\'>jour/mois/année</span>'
+        }
 
 
 
