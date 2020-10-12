4248adab49c1a326016ac66ddb9fd396236e1d54
AdamMusa <adammusa2222@gmail.com>
2020-10-12 11:03:36 +0100

    modification apporté par valery

--- genoskul/views.py
+++ genoskul/views.py
@@ -12,10 +12,10 @@ class HomeTemplateView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['jobs'] = Job.objects.all()[:3]
+        context['jobs'] = Job.objects.all()[3:]
         context['categories'] = Document.objects.all()[:5]
         context['documents'] = DocumentCategory.objects.all()[:10]
-        context['posts'] = Post.objects.all()[:10]
+        context['posts'] = Post.objects.all()[10:]
         context['job_categories'] = JobCategory.objects.all()[:10]
         context['services'] = Box.objects.get_confirmed_services()
         context['footer'] = Footer.objects.last()
