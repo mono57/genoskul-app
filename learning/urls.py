480f1313b3db93667253e0a2da011891c513d292
AdamMusa <adammusa2222@gmail.com>
2020-12-03 12:04:10 +0100

    change url

--- learning/urls.py
+++ learning/urls.py
@@ -4,20 +4,20 @@ from learning.views import *
 app_name = 'learning'
 
 urlpatterns = [
-     path('document/create/',
-          DocumentCreateView.as_view(),
-          name='document'),
-     path('documents/list/',
-         DocumentListView.as_view(), name='document-list'),   
+    path('document/create/',
+         DocumentCreateView.as_view(),
+         name='document'),
+    path('documents/list/',
+         getDocuments, name='document-list'),
 
-     path('doc_categories/list/',
+    path('doc_categories/list/',
          DocumentCategoryListView.as_view(),
          name='doc_category-list'),
-         
-     path('doc_categories/<int:pk>/detail/',
+
+    path('doc_categories/<int:pk>/detail/',
          CategoryDetailView.as_view(), name='doc_categories'),
-    
-     path('courses/', CourseListView.as_view(), name='course-list')
-   
+
+    path('courses/', CourseListView.as_view(), name='course-list')
+
 
 ]
