0b90f5c24faa004a2cf205ae6077e10248078700
mono57 <aymaramono@gmail.com>
2021-04-05 15:55:41 +0100

    random services and add welcome text to forum

--- genoskul/views.py
+++ genoskul/views.py
@@ -9,6 +9,15 @@ from django.urls import reverse
 
 import random
  
+
+def get_random_list(queryset):
+    r_list = []
+    for _ in range(len(queryset)):
+        choice = random.choice(queryset)
+        r_list.append(choice)
+        queryset.remove(choice)
+
+    return r_list
 class HomeTemplateView(TemplateView):
     template_name = 'index.html'
 
@@ -19,19 +28,12 @@ class HomeTemplateView(TemplateView):
         context['documents'] = DocumentCategory.objects.all()[:10]
         context['document'] = Document.objects.all()[:10]
         context['job_categories'] = JobCategory.objects.all()[:10]
-        context['services'] = Box.objects.get_confirmed_services().order_by('-created_at')
+        context['services'] = get_random_list(
+            list(Box.objects.get_confirmed_services().order_by('-created_at')))
         context['footer'] = Footer.objects.last()
         context['social_networks'] = SocialNetwork.objects.all()
         context['about'] = About.objects.last()
-
-        posts = list(Post.objects.all().order_by('-created_at'))[:21]
-
-        r_posts = []
-        for _ in range(len(posts)):
-            choice = random.choice(posts)
-            r_posts.append(choice)
-            posts.remove(choice)
-
-        context['posts'] = r_posts
+        context['posts'] = get_random_list(
+            list(Post.objects.all().order_by('-created_at'))[:21])
 
         return context
